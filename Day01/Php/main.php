6d54c63562ca50e421f75ed0a6950fe2925ee783
Rhainas <leo.viltard@epitech.eu>
2023-12-01 21:22:45 +0100

    [~][PHP] Day01 fix parsing

--- Day01/Php/main.php
+++ Day01/Php/main.php
@@ -13,22 +13,24 @@ function openFile($filename)
 function getNumbers($input, $wordsToFind)
 {
     $res = array(-1, -1);
-    for ($i = 0, $j = strlen($input) - 1; $i < strlen($input); $i++, $j--) {
-        if (ctype_digit($input[$i]))
+
+    for ($i = 0, $j = strlen($input) - 1; $i < strlen($input) && ($res[0] == -1 || $res[1] == -1); $i++, $j--) {
+
+        if (ctype_digit($input[$i]) && $res[0] == -1)
             $res[0] = $input[$i];
-        if (ctype_digit($input[$j]))
+        if (ctype_digit($input[$j]) && $res[1] == -1)
             $res[1] = $input[$j];
 
         foreach ($wordsToFind as $index => $word) {
-            if (substr($input, $i, strlen($word)) == $word)
+            if (substr($input, $i, strlen($word)) == $word && $res[0] == -1)
                 $res[0] = $index + 1;
-            if (substr($input, $j - strlen($word) + 1, strlen($word)) == $word)
+            if (substr($input, $j - strlen($word) + 1, strlen($word)) == $word && $res[1] == -1)
                 $res[1] = $index + 1;
         }
     }
     if ($res[0] == -1 || $res[1] == -1)
         return "";
-    return $res[1] . $res[0];
+    return $res[0] . $res[1];
 }
 
 function parseArray($input)
